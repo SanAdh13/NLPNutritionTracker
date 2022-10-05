bef4cf246e8db97d35314a986327be23b46bc760
SanAdh13 <sanrakshakadhikari@gmail.com>
2022-10-05 06:24:39 +0100

    added basic layout of frontend, might have to play around with the _navbar bc i dont want to be able to add food when in /track; tbh the modal doesnt load in that page anyways

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request,url_for
 from flaskext.markdown import Markdown
+from flask_modals import Modal 
 
 from spacy import displacy
 
@@ -9,16 +10,15 @@ import NER as ner
 
 app = Flask(__name__)
 Markdown(app)
-
 #landing page
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/addFood")
-def food():
-    return 0
+# @app.route("/addFood")
+# def food():
+#     return render_template("addFood.html")
 
 @app.route('/getVoiceRecording',methods=['GET','POST'])
 def getVoiceRecording():
@@ -26,18 +26,15 @@ def getVoiceRecording():
         ''' 
             TODO: we will take the voice clip recorded via JS
             and pass it for speech.speech() in order to get {text}  
-            and then use ner.ner({text})
+            and then usse ner.ner({text})
             use displacy to show the extracted ENT
             and then add the relevant ents to db         
         '''
 
 
-
-
 @app.route("/track")
 def track():
-    return 0
-
+    return render_template("track.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
